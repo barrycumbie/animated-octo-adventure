d264a87be1a1150a038bde6d7f8232376799ddaf
Cumbie <bcumbie@una.edu>
2022-01-25 16:45:55 -0600

    adding in node xp and a new dev-team com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DevTeamComponent } from './dev-team/dev-team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DevTeamComponent
   ],
   imports: [
     BrowserModule
