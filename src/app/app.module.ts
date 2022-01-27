35ec3c2cf4f54f9a90ef9e4305cb632f019b0036
Cutshall <zcutshall@una.edu>
2022-01-27 16:52:12 -0600

    added component-schedule to app and referenced component-schedule.html to app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DevTeamComponent } from './dev-team/dev-team.component';
+import { ComponentScheduleComponent } from './component-schedule/component-schedule.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DevTeamComponent
+    DevTeamComponent,
+    ComponentScheduleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component-schedule/component-schedule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-schedule',
+  templateUrl: './component-schedule.component.html',
+  styleUrls: ['./component-schedule.component.css']
+})
+export class ComponentScheduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
