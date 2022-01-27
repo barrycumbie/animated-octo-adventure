115c852b238338d68bf6b6c9a6a563f107914751
Brown <egiles@una.edu>
2022-01-27 16:52:07 -0600

    create weather components and the weather component is in app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DevTeamComponent } from './dev-team/dev-team.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DevTeamComponent
+    DevTeamComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
