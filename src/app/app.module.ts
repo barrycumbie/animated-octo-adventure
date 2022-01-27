b247cd0a03f75a98a5470854c1d704e69b72ed31
Esther Giles <47467263+esthergiles@users.noreply.github.com>
2022-01-27 17:02:32 -0600

    create weather components and the weather component is in app.component.html (#148)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DevTeamComponent } from './dev-team/dev-team.component';
+<<<<<<< HEAD
+import { WeatherComponent } from './weather/weather.component';
+=======
 import { PolicyFinderComponent } from './policy-finder/policy-finder.component';
+>>>>>>> f129f4298760ece88091b76c0c634c3d5cb8ac0f
 
 @NgModule({
   declarations: [
     AppComponent,
     DevTeamComponent,
+<<<<<<< HEAD
+    WeatherComponent
+=======
     PolicyFinderComponent
+>>>>>>> f129f4298760ece88091b76c0c634c3d5cb8ac0f
   ],
   imports: [
     BrowserModule
