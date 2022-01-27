2b9ea97951ca8042638bad647cd7734078052b06
Coplen <bcoplen@una.edu>
2022-01-27 16:38:46 -0600

    adding tag component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DevTeamComponent } from './dev-team/dev-team.component';
 import { ContactComponent } from './contact/contact.component';
+import { TagComponentComponent } from './tag-component/tag-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DevTeamComponent,
-    ContactComponent
+    ContactComponent,
+    TagComponentComponent
   ],
   imports: [
     BrowserModule
