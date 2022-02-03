d7addc66a805c319aeec0f507b68ee1762918dfb
Brandon Coplen <89221350+brandoncoplen@users.noreply.github.com>
2022-02-03 16:15:03 -0600

    adding tag component (#146)
    * adding tag component
    
    * profile
    
    * adding tag component
    
    * adding our tag to app.component.html
    
    * mergin origin
    
    * added a link to my html
    
    * added our footer component
    
    * added footer tag to app.component
    
    Co-authored-by: Coplen <bcoplen@una.edu>
    Co-authored-by: srthorne <srthorne@gmail.com>
    Co-authored-by: King <jking18@una.edu>
    Co-authored-by: Cumbie <bcumbie@una.edu>
    Co-authored-by: Uriel Malewo <urielmalewo@uriels-mbp.main.local.una.edu>
    Co-authored-by: Uriel Malewo <urielmalewo@Uriels-MacBook-Pro.local>
    Co-authored-by: srthorne <89217674+srthorne@users.noreply.github.com>
    Co-authored-by: Katrina <55498867+KatrinaYates@users.noreply.github.com>
    Co-authored-by: Katrina <Krmoore014@gmail.com>
    Co-authored-by: Cutshall <zcutshall@una.edu>

---
+++ src/app/tag-component/tag-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-component',
+  templateUrl: './tag-component.component.html',
+  styleUrls: ['./tag-component.component.css']
+})
+export class TagComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
