deab11ce60b2759939be7565b09dd90bbf23eeca
Uriel Malewo <urielmalewo@Uriels-MacBook-Pro.local>
2022-02-03 15:14:58 -0600

    added our footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { DevTeamComponent } from './dev-team/dev-team.component';
 import { WeatherComponent } from './weather/weather.component';
 import { PolicyFinderComponent } from './policy-finder/policy-finder.component';
+import { ComponentScheduleComponent } from './component-schedule/component-schedule.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DevTeamComponent,
     WeatherComponent,
-    PolicyFinderComponent
+    PolicyFinderComponent,
+    ComponentScheduleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
