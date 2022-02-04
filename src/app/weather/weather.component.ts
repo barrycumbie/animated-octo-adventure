703455a50294e9ef2adb469a99a3727049846c3a
vanpham2000 <86323153+vanpham2000@users.noreply.github.com>
2022-02-04 17:17:01 -0600

    Revert "Iss168 (#169)"
    This reverts commit 1ba548b923408bdc3ab298e44d542dfcc3bad62b.

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,28 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Weather} from '../app.component';
-
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
+export class WeatherComponent implements OnInit {
 
-export class WeatherComponent {
-  weather: Weather = {
-  city: "Florence",
-  conditions: "Sunny",
-  temperature: 70,
-  icon: ""
+  constructor() { }
+
+  ngOnInit(): void {
   }
+
 }
-
-
-// export class WeatherComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
